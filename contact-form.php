31cea6561e25d3901e4812e7fb1cbd247e6ac739
Aaron E. Lebel <37777357+alebml@users.noreply.github.com>
2018-05-14 20:38:18 -0400

    Added About.html, edited index

---
+++ contact-form.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['button'])) {
+	$first = $_POST['firstname'];
+	$last = $_POST['lastname'];
+	$email = $_POST['email'];
+
+	$mailto = "alebel@andrew.cmu.edu";
+	$subject = "New Email Form Entry for CNT";
+	$headers = "From: ".$first;
+	$txt = "New email form user: ".$email;
+
+	mail($mailto, $subject, $txt, $headers);
+	header("Location: index.php?mailsend");
+}
