06893eb98f76087ca6a7e9f90afa12037f23f8b7
alebml <37777357+alebml@users.noreply.github.com>
2018-07-03 11:29:22 -0400

    WhitePaperImplementation

---
+++ dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServerName = "localhost";
+$dbUsername = "endricmy_alebml";
+$dbPassword = "Silver54";
+$dbName = "users";
+
+$conn = mysqli_connect($dbServerName,$dbUsername,$dbPassword,$dbName);
